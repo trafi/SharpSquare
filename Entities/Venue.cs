ebf1a99d5c41c4870c1e2b121f96dbc730b73637
Karolis Vycius <karolis@trafi.com>
2016-06-27 10:34:05 +0300

    Add suggest completeion for Foursquare

--- Entities/Venue.cs
+++ Entities/Venue.cs
@@ -2,28 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SharpSquare.Entities;
 
 namespace FourSquare.SharpSquare.Entities
 {
-    public class Venue : FourSquareEntity
+    public class Venue : MiniVenue
     {
-        /// <summary>
-        /// A unique string identifier for this venue.
-        /// </summary>
-        public string id
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// The best known name for this venue.
-        /// </summary>
-        public string name
-        {
-            get;
-            set;
-        }
 
         /// <summary>
         /// An object containing none, some, or all of twitter, phone, and formattedPhone. All are strings.
@@ -34,24 +18,6 @@ namespace FourSquare.SharpSquare.Entities
             set;
         }
 
-        /// <summary>
-        ///  An object containing none, some, or all of address (street address), crossStreet, city, state, postalCode, country, lat, lng, and distance. All fields are strings, except for lat, lng, and distance. Distance is measured in meters. 
-        ///  Some venues have their locations intentionally hidden for privacy reasons (such as private residences). If this is the case, the parameter isFuzzed will be set to true, and the lat/lng parameters will have reduced precision. 
-        /// </summary>
-        public Location location
-        {
-            get;
-            set;
-        }
-
-        /// <summary>
-        /// An array, possibly empty, of categories that have been applied to this venue. One of the categories will have a field primary indicating that it is the primary category for the venue. For the complete set of categories, see venues/categories. 
-        /// </summary>
-        public List<Category> categories
-        {
-            get;
-            set;
-        }
 
         /// <summary>
         /// Boolean indicating whether the owner of this business has claimed it and verified the information.
